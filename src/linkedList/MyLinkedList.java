262e266355c89f1e71432e0c2320c0ce026924fe
KimBabBo <mayh1130@naver.com>
2024-09-16 11:01:25 +0900

    Linked List First Study

---
+++ src/linkedList/MyLinkedList.java
@@ -0,0 +1,128 @@
+package linkedList;
+
+public class MyLinkedList {
+    private int data;
+    private MyLinkedList next;
+
+
+    public MyLinkedList(int data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public MyLinkedList(int data, MyLinkedList next) {
+        this.data = data;
+        this.next = next;
+    }
+
+    public int getData() {
+        return data;
+    }
+    public MyLinkedList getNext() {
+        return next;
+    }
+    public void setNext(MyLinkedList next) {
+        this.next = next;
+    }
+
+    private MyLinkedList head;
+    private int count;
+
+    public MyLinkedList() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public MyLinkedList getHead() {
+        return head;
+    }
+
+    public void insertAt(int index, int data) {
+        if (index < 0 || index > count) {
+            throw new IndexOutOfBoundsException("범위를 벗어났습니다.");
+        }
+
+        MyLinkedList newNode = new MyLinkedList(data);
+
+        if (index == 0) {
+            newNode.setNext(this.head);
+            this.head = newNode;
+        } else {
+            MyLinkedList currentNode = this.head;
+            for (int i = 0; i < index - 1; i++) {
+                currentNode = currentNode.getNext();
+            }
+            newNode.setNext(currentNode.getNext());
+            currentNode.setNext(newNode);
+        }
+        count++;
+    }
+
+    public void printAll(){
+        MyLinkedList currentIndex = this.head;
+        String text = "[";
+        while (currentIndex != null) {
+            text += currentIndex.getData();
+            currentIndex = currentIndex.getNext();
+
+            if (currentIndex != null) {
+                text += ", ";
+            }
+        }
+        text += "]";
+
+        System.out.println(text);
+    }
+
+    public void clear(){
+        this.head = null;
+        this.count = 0;
+    }
+
+    public void insertLast(int data) {
+        this.insertAt(this.count, data);
+    }
+
+    public MyLinkedList deleteAt(int index) {
+        if(index < 0 || index > count) {
+            throw new IndexOutOfBoundsException("제거할 수 없습니다.");
+        }
+        MyLinkedList currentIndex = this.head;
+        if(index == 0) {
+            MyLinkedList deleteNode = this.head;
+            this.head = this.head.getNext();
+            this.count--;
+            return deleteNode;
+        } else {
+            for(int i = 0; i < index - 1; i++) {
+                currentIndex = currentIndex.getNext();
+            }
+            MyLinkedList deleteNode = currentIndex.getNext();
+            currentIndex.setNext(currentIndex.getNext().getNext());
+            this.count--;
+            return deleteNode;
+        }
+    }
+
+    public MyLinkedList deleteLast() {
+        return this.deleteAt(this.count - 1);
+    }
+
+    public MyLinkedList getNodeAt(int index) {
+        if (index < 0 || index > this.count) {
+            throw new IndexOutOfBoundsException("범위를 넘어갔습니다.");
+        }
+
+        MyLinkedList currentIndex = this.head;
+        for (int i = 0; i < index; i++) {
+            currentIndex = currentIndex.getNext();
+        }
+
+        return currentIndex;
+    }
+
+}
---
+++ src/main/Main.java
@@ -0,0 +1,71 @@
+package main;
+
+import java.util.LinkedList;
+import linkedList.MyLinkedList;
+import org.w3c.dom.Node;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList node1 = new MyLinkedList(1);
+        MyLinkedList node2 = new MyLinkedList(2);
+        MyLinkedList node3 = new MyLinkedList(3);
+
+        node1.setNext(node2);
+        node2.setNext(node3);
+
+        System.out.println(node1.getData());
+        System.out.println(node1.getNext().getData());
+        System.out.println(node1.getNext().getNext().getData());
+
+        System.out.println("================= printList () ==================");
+
+        MyLinkedList list = new MyLinkedList();
+
+        list.insertAt(0, 0);
+        list.insertAt(1, 1);
+        list.insertAt(2, 2);
+        list.insertAt(3, 3);
+        list.insertAt(4, 4);
+        list.printAll();
+
+        System.out.println("================= clear () ==================");
+
+        list.clear();
+        list.printAll();
+
+        System.out.println("================= clear () =================");
+        list.insertLast(0);
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.printAll();
+
+        System.out.println("================= deleteAt () =================");
+
+        list.deleteAt(0);
+        list.printAll();
+        list.deleteAt(1);
+        list.printAll();
+
+        System.out.println("================= deleteLast () =================");
+
+        list.insertLast(5);
+        list.deleteLast();
+        list.deleteLast();
+        list.deleteLast();
+        //list.deleteLast();  //오류동작 확인
+        list.printAll();
+
+        System.out.println("================= deleteLast () =================");
+
+        list.insertLast(1);
+        list.insertLast(2);
+        list.insertLast(3);
+        list.insertLast(4);
+        list.insertLast(5);
+
+        MyLinkedList secondNode = list.getNodeAt(2);
+        System.out.println(secondNode.getData());
+    }
+
+}
